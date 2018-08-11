c1e50d12ade8fe2fd172dbab4eaf98f4470f7452
shineframe <41407614+shineframe@users.noreply.github.com>
2018-08-11 16:46:40 +0800

    FIX数字解码BUG

--- include/serial/serial.hpp
+++ include/serial/serial.hpp
@@ -139,7 +139,77 @@ namespace shine
             e_map = 8,
             e_struct = 9,
         };
+
+        template<typename T>
+        inline void encode_size(shine::string &buf, T size){
+            do {
+                shine::int8 ch = size & ((1 << 7) - 1);
+                if (size >>= 7)
+                    ch |= 0x80;
+
+                buf += ch;
+            } while (size);
+        }
+
+        template<typename T>
+        inline shine::size_t decode_size(T &val, const shine::int8 *data, const shine::size_t len){
+            if (len < 1)
+                return 0;
+
+            val = 0;
+            const shine::int8 *p = data;
+            shine::size_t i = 0;
+
+            for (;;)
+            {
+                if ((p[i] & 0x80) == 0x80)
+                {
+                    if (i < len - 1)
+                    {
+                        val += (p[i] & ((1 << 7) - 1)) * (1 << 7 * i);
+                        i++;
+                    }
+                    else
+                        return 0;
+                }
+                else
+                {
+                    val += (p[i] & ((1 << 7) - 1)) * (1 << 7 * i);
+                    return i + 1;
+                }
+            }
+        }
+
     }
+
+    struct package{
+        uint32 length = 0;
+        uint32 type = 0;
+
+        inline string encode(){
+            string ret;
+
+            serial::encode_size(ret, length);
+            serial::encode_size(ret, type);
+
+            return std::move(ret);
+        }
+
+        inline size_t decode(const int8 *data, const size_t len){
+            size_t cost_len = serial::decode_size(length, data, len);
+
+            if (cost_len == 0)
+                return 0;
+
+            size_t cost_len2 = serial::decode_size(type, data + cost_len, len - cost_len);
+
+            if (cost_len2 == 0)
+                return 0;
+
+            return cost_len + cost_len2;
+        }
+    };
+
 }
 
 inline void serial_encode_type(shine::int8 type, shine::string &buf){
@@ -168,20 +238,28 @@ inline bool serial_decode_size(shine::size_t &val, const shine::int8 *data, cons
         return false;
 
     val = 0;
-
     const shine::int8 *p = data + cost_len;
-
     shine::size_t i = 0;
-    for (; (p[i] & 0x80) == 0x80 && cost_len + i < len; i++)
-        val += (p[i] & ((1 << 7) - 1)) * (1 << 7 * i);
 
-    if (cost_len + i > len)
-        return false;
-
-    val += (p[i] & ((1 << 7) - 1)) * (1 << 7 * i);
-
-    cost_len += i + 1;
-    return true;
+    for (;;)
+    {
+        if ((p[i] & 0x80) == 0x80)
+        {
+            if (i < len - cost_len - 1)
+            {
+                val += (p[i] & ((1 << 7) - 1)) * (1 << 7 * i);
+                i++;
+            }
+            else
+                return false;
+        }
+        else
+        {
+            val += (p[i] & ((1 << 7) - 1)) * (1 << 7 * i);
+            cost_len += i + 1;
+            return true;
+        }
+    }
 }
 
 template<class T>
@@ -218,15 +296,26 @@ bool serial_decode_integer(T &val, const shine::int8 *data, const shine::size_t 
 
     const shine::int8 *p = data + cost_len + 1;
     shine::Bool flag = (p[0] & 0x40) == 0x40;
-
     shine::size_t i = 0;
-    for (; (p[i] & 0x80) == 0x80 && cost_len + i < len; i++)
-        val += (p[i] & ((1 << 6) - 1)) * (1 << 6 * i);
 
-    if (cost_len + i + 1 > len)
-        return false;
-
-    val += (p[i] & ((1 << 6) - 1)) * (1 << 6 * i);
+    for (;;)
+    {
+        if ((p[i] & 0x80) == 0x80)
+        {
+            if (i < len - cost_len - 1)
+            {
+                val += (p[i] & ((1 << 6) - 1)) * (1 << 6 * i);
+                i++;
+            }
+            else
+                return false;
+        }
+        else
+        {
+            val += (p[i] & ((1 << 6) - 1)) * (1 << 6 * i);
+            break;
+        }
+    }
 
     if (flag)
         val = -val;
