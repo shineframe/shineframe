f4d47f7d9d8abcc29e0fbe14452b45f5fa6ca7c1
shineframe <shineframe@github.com>
2019-06-06 11:48:15 +0800

    simple pool thread safe

--- include/util/pool.hpp
+++ include/util/pool.hpp
@@ -10,64 +10,69 @@ using namespace std;
 
 namespace shine
 {
-    namespace pool
-    {
-        template<class T>
-        class simple
-        {
-        public:
-            simple(uint32 bluck_size = 10000, bool auto_expand = true)
-            {
-                _bluck_size = bluck_size;
-                _auto_expand = auto_expand;
-            }
+	namespace pool
+	{
+		template<class T>
+		class simple
+		{
+		public:
+			simple(uint32 bluck_size = 10240, bool auto_expand = true)
+			{
+				_bluck_size = bluck_size;
+				_auto_expand = auto_expand;
+			}
 
-            ~simple(){
-                clear();
-            }
+			~simple() {
+				clear();
+			}
 
-            T* take(){
-                if (_elements.empty())
-                {
-                    if (!_auto_expand)
-                        return nullptr;
-                    expand_bluck();
-                }
+			T* take() {
+				std::unique_lock<std::recursive_mutex> lock(_mutex);
+				if (_elements.empty())
+				{
+					if (!_auto_expand)
+						return nullptr;
+					expand_bluck();
+				}
 
-                T *ret = _elements.front();
-                _elements.pop_front();
-                return ret;
-            }
+				T *ret = _elements.front();
+				_elements.pop_front();
+				return ret;
+			}
 
-            void put(T* item){
-                _elements.push_front(item);
-            }
+			void put(T* item) {
+				std::unique_lock<std::recursive_mutex> lock(_mutex);
+				_elements.push_front(item);
+			}
 
-            void clear(){
-                for (auto bluck : _blucks)
-                {
-                    delete[] bluck;
-                }
+			void clear() {
+				std::unique_lock<std::recursive_mutex> lock(_mutex);
+				for (auto bluck : _blucks)
+				{
+					delete[] bluck;
+				}
 
-                _blucks.clear();
-                _elements.clear();
-            }
+				_blucks.clear();
+				_elements.clear();
+			}
 
-        private:
-            void expand_bluck(){
-                T *bluck = new T[_bluck_size];
-                for (uint32 i = 0; i < _bluck_size; i++)
-                    _elements.push_front(bluck + i);
-                _blucks.push_front(bluck);
-            }
+		private:
+			void expand_bluck() {
+				std::unique_lock<std::recursive_mutex> lock(_mutex);
+				T *bluck = new T[_bluck_size];
+				for (uint32 i = 0; i < _bluck_size; i++)
+					_elements.push_front(bluck + i);
+				_blucks.push_front(bluck);
+			}
 
-        private:
-            std::forward_list<T*> _elements;
-            std::forward_list<T*> _blucks;
-            uint32 _bluck_size = 0;
-            bool _auto_expand = true;
-        };
-    }
+		private:
+			std::forward_list<T*> _elements;
+			std::forward_list<T*> _blucks;
+			uint32 _bluck_size = 0;
+			bool _auto_expand = true;
+			std::recursive_mutex _mutex;
+		};
+	}
 
 }
 
