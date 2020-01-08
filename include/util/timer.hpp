d939b3b971cab2fc283356c21ffe03f2fab15848
shineframe <shineframe@github.com>
2020-01-08 21:53:09 +0800

    timer提供精确与相对两种模式

--- include/util/timer.hpp
+++ include/util/timer.hpp
@@ -19,8 +19,9 @@ namespace shine
         SHINE_GEN_MEMBER_GETSET(uint64, id);
         SHINE_GEN_MEMBER_GETSET(uint64, delay);
         SHINE_GEN_MEMBER_GETSET(uint64, timestamp);
-        SHINE_GEN_MEMBER_GETSET(timer_callback_t, callback);
-    };
+		SHINE_GEN_MEMBER_GETSET(timer_callback_t, callback);
+		SHINE_GEN_MEMBER_GETSET(bool, relative);
+	};
 
     class timer_manager : std::multimap <uint64, timer>{
     public:
@@ -48,8 +49,18 @@ namespace shine
                     erase(iter);
                     bool repeat = item.get_callback()();
                     if (repeat) {
-                        item.set_timestamp(now + item.get_delay());
-                        insert(std::make_pair(item.get_timestamp(), item));
+						now = tool::get_timestamp();
+
+						if (item.get_relative()) {
+							item.set_timestamp(now + item.get_delay());
+						}
+						else {
+							auto ts = item.get_timestamp() + item.get_delay();
+							if (ts < now)
+								ts = now;
+							item.set_timestamp(ts);
+						}
+						insert(std::make_pair(item.get_timestamp(), item));
                     }
 
                 }
@@ -65,17 +76,22 @@ namespace shine
         /** 
          *@brief 新增一个定时任务
          *@param delay 延迟时间，单位毫秒
-         *@param cb 回调对象
-         *@return shine::uint64 返回定时任务id
+		 *@param cb 回调对象
+		 *@param relative 是否为相对时间触发
+		 *@return shine::uint64 返回定时任务id
          *@warning 
          *@note 
         */
-        uint64 set_timer(uint64 delay, timer_callback_t cb) {
-            timer item;
+        uint64 set_timer(uint64 delay, timer_callback_t cb, bool relative = false) {
+			if (delay == 0)
+				return invalid_timer_id;
+
+			timer item;
             item.set_id(++_id);
             item.set_delay(delay);
             item.set_timestamp(tool::get_timestamp() + delay);
             item.set_callback(std::move(cb));
+			item.set_relative(relative);
 
             insert(std::make_pair(item.get_timestamp(), item));
             return item.get_id();
